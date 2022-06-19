6788bc357133a072f8edd65b8f62a0c5fcf7c9f5
albjimmar1 <47810351+albjimmar1@users.noreply.github.com>
2022-06-19 20:16:05 +0200

    static html rendering 10

--- index.js
+++ index.js
@@ -39,7 +39,12 @@ express.use('/auth-session', authSessionRouter);
 express.use('/auth-token', authTokenRouter);
 
 const bootstrap = async () => { 
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    },);
 
     express.listen(PORT, () =>
         console.log('Server on port %d', PORT)
